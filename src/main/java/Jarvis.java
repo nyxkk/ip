8f775812c48635dabcb254e98c6065ef8a45efde
Nix Koh <nixkoh8@gmail.com>
2026-08-19 16:32:23 +0800

    Complete Level-1: echo and bye

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * A simple command-line assistant that echoes commands until the user says goodbye.
+ */
 public class Jarvis {
+    /**
+     * Starts Jarvis and reads commands from standard input.
+     *
+     * @param args command-line arguments, which are not used
+     */
     public static void main(String[] args) {
         String banner = """
                      ██╗ █████╗ ██████╗ ██╗   ██╗██╗███████╗
@@ -7,15 +17,28 @@ public class Jarvis {
                 ██   ██║██╔══██║██╔══██╗╚██╗ ██╔╝██║╚════██║
                 ╚█████╔╝██║  ██║██║  ██║ ╚████╔╝ ██║███████║
                  ╚════╝ ╚═╝  ╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚══════╝
-                """.stripTrailing().substring(1);
+                """.stripTrailing();
 
         String separator = "_".repeat(60);
         System.out.println(separator);
-        System.out.println(banner);
+        System.out.println(banner.substring(1));
         System.out.println("Hello! I'm Jarvis.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+
+            System.out.println(separator);
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println(" " + command);
+            System.out.println(separator);
+        }
     }
 }
