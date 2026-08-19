10598526fe668f5ea76517e28f1fa05b9c17eab7
Nix Koh <nixkoh8@gmail.com>
2026-08-19 16:43:46 +0800

    Complete Level-2: add, list

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -27,6 +27,9 @@ public class Jarvis {
         System.out.println(separator);
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while (true) {
             String command = scanner.nextLine();
 
@@ -35,9 +38,16 @@ public class Jarvis {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(separator);
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println(" added: " + command);
             }
 
-            System.out.println(" " + command);
             System.out.println(separator);
         }
     }
